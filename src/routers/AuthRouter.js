61e55870934ffeedec6ed99fe9daa4601db9dde8
alex <ftvalex@hotmail.com>
2022-05-23 11:10:50 +0200

    commit Login
     estilos del login

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
 
-   
+
 
 
   return (
-    <div>
-    <Routes>
-        <Route path="/login" element={<LoginScreen/>}></Route>
-        <Route path ="/register" element={<RegisterScreen/>}></Route>
-        <Route path ="*" element={<LoginScreen/>}/>
-    </Routes>
+    <div className='auth__main'>
+      <div className='auth__box-container'>
+        <Routes>
+          <Route path="/login" element={<LoginScreen />}></Route>
+          <Route path="/register" element={<RegisterScreen />}></Route>
+          <Route path="*" element={<LoginScreen />} />
+        </Routes>
+      </div>
     </div>
   )
 }
